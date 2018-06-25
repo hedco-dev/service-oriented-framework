d4f51786bb9b7e8503dba30a065ddf5e03d4f275
Majid Parvin <parvin.majid@hedcoint.com>
2018-06-25 20:27:54 +0430

    add logo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
+const chalk = require('chalk');
+const figlet = require('figlet');
+const clear = require('clear');
 
 global.rootPath = __dirname;
 require('./bootstrap')
   .then(() => {
     app.listen(global.config.http.port, () => {
-      logger.log(`Server has been started on ${global.config.http.port}`);
+      clear();
+      logger.log(
+        chalk.red(
+          figlet.textSync('MAGIC', {
+            horizontalLayout: 'full',
+            font: 'poison'
+          })
+        )
+      );
+      logger.log(chalk.red(`Server has been started on ${global.config.http.port}`));
     });
   }).catch(err => {
     console.log(err);
