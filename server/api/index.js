70d067defefed494ba452df4cb8e90fe06f21e01
Majid Parvin <parvin.majid@hedcoint.com>
2018-07-02 17:09:36 +0430

    binding *Model.js as model

--- server/api/index.js
+++ server/api/index.js
@@ -1,41 +1,61 @@
-const express = require('express');
-const { lstatSync, readdirSync } = require('fs');
+import * as express from 'express';
+const { lstatSync, readdirSync, existsSync } = require('fs');
 const { join, basename } = require('path');
 const includeAll = require('include-all');
 const isDirectory = source => lstatSync(source).isDirectory();
 const findController = (path) => includeAll({
-    dirname: path,
-    filter: /(.*Controller)\.js$/,
-    depth: 1
+  dirname: path,
+  filter: /(.*Controller)\.js$/,
+  depth: 1
+});
+const findModel = (path) => includeAll({
+  dirname: path,
+  filter: /(.*Model)\.js$/,
+  depth: 1
 });
 const bindRoutes = (funcName, controller, controllerRouter) => {
-    const httpMothods = ["get", "post", "put", "delete"];   
-    httpMothods.forEach((httpMethod) => {
-        if (funcName.startsWith(httpMethod)) {
-            controllerRouter.route(`/${funcName.replace(httpMethod, '')}`)[httpMethod](controller[funcName]);
-        }
-    });
+  const httpMothods = ["get", "post", "put", "delete"];
+  httpMothods.forEach((httpMethod) => {
+    if (funcName.startsWith(httpMethod)) {
+      controllerRouter.route(`/${funcName.replace(httpMethod, '')}`)[httpMethod](controller[funcName]);
+    }
+  });
 };
 module.exports = (app) => {
-    const apiPath = magic.rootPath + '/api';
-    readdirSync(apiPath).map(name => join(apiPath, name))
-        .forEach((d) => {
-            if (isDirectory(d)) {
-                const controllers = findController(d);
-                const folderName = basename(d);
-                Object.keys(controllers).filter((ctrl) => controllers[ctrl]).forEach((ctrl) => {
-                    var apiPath = `/${folderName}`;
-                    const ctrlName = ctrl.replace("Controller", "");
-                    if (ctrlName) {
-                        apiPath += `/${ctrlName}`;
-                    }
-                    const controller = controllers[ctrl];
-                    const apis = Object.keys(controller);
+  const apiPath = magic.rootPath + '/api';
+  magic.models = {};
+  readdirSync(apiPath)
+    .map(name => join(apiPath, name))
+    .filter(d => isDirectory(d))
+    .forEach((d) => {
+      const controllers = findController(d);
+      const folderName = basename(d);
+      Object.keys(controllers)
+        .filter((ctrl) => controllers[ctrl])
+        .forEach((ctrl) => {
+          var rootPath = `/${folderName}`;
+          const ctrlName = ctrl.replace("Controller", "");
+          if (ctrlName) {
+            rootPath += `/${ctrlName}`;
+          }
+          const controller = controllers[ctrl];
+          const apis = Object.keys(controller);
 
-                    const controllerRouter = express.Router();
-                    apis.forEach((funcName) => bindRoutes(funcName, controller, controllerRouter));
-                    app.use(apiPath, controllerRouter);
-                });
-            }
+          const controllerRouter = express.Router();
+          apis.forEach((funcName) => bindRoutes(funcName, controller, controllerRouter));
+          app.use(rootPath, controllerRouter);
         });
+
+      // resolve models
+      const models = findModel(d);
+      Object.keys(models)
+        .filter((model) => models[model])
+        .forEach((model) => {
+          const modelName = model.replace("Model", "");
+          if (modelName) {
+            magic.models[folderName] = magic.models[folderName] || {};
+            magic.models[folderName][modelName] = require(`${d}/${model}.js`);
+          }
+        });
+    });
 };
