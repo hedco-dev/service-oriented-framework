dc9c41ec135da9e1d40f351c399e3991b4e0c714
Majid Parvin <parvin.majid@hedcoint.com>
2018-07-06 16:03:30 +0430

    manage application events

--- server/index.js
+++ server/index.js
@@ -1,31 +1,64 @@
 import express from 'express';
 import chalk from 'chalk';
 import figlet from 'figlet';
+import http from 'http';
 import middlewares from './middlewares';
 import apis from './api';
 import bootstrap from './bootstrap';
+import { close } from './utils/db';
 
 const app = express();
+const server = http.createServer(app);
 global.magic = global.magic || {};
 magic.rootPath = __dirname;
+
+const onExitHandler = async () => {
+  try {
+    server.close();
+    process.exit();
+    await close();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default (async () => {
   await bootstrap();
   middlewares(app);
   apis(app);
-  
-  app.listen(magic.config.http.port, () => {
-    console.log(
-      chalk.red(
-        figlet.textSync('magic', {
-          horizontalLayout: 'full',
-          font: 'standard'
-        })
-      )
-    );
-    magic.logger.info(chalk.white(`Environment: ${magic.config.environment.env}`));
-    magic.logger.info(chalk.white(`Start Date:  ${new Date()}`));
-    magic.logger.warning(`Framework Version:  ${magic.config.environment.version}`);
-    magic.logger.success(chalk.white(`Server has been started on ${magic.config.http.port}`));
+
+  const connect = () => {
+    server.listen(magic.config.http.port, () => {
+      console.log(
+        chalk.red(
+          figlet.textSync('magic', {
+            horizontalLayout: 'full',
+            font: 'standard'
+          })
+        )
+      );
+      magic.logger.info(chalk.white(`Environment: ${magic.config.environment.env}`));
+      magic.logger.info(chalk.white(`Start Date:  ${new Date()}`));
+      magic.logger.warning(`Framework Version:  ${magic.config.environment.version}`);
+      magic.logger.success(chalk.white(`Server has been started on ${magic.config.http.port}`));
+
+      app.on('error', onExitHandler.bind(null, { error: true }));
+      process.on('exit', onExitHandler.bind(null, { exit: true }));
+      process.on('SIGINT', onExitHandler.bind(null, { SIGINT: true }));
+      process.on('SIGTERM', onExitHandler.bind(null, { SIGTERM: true }));
+      //process.on('uncaughtException', onExitHandler.bind(null, { uncaughtException: true }));
+      //process.on('SIGKILL', onExitHandler.bind(null, { SIGKILL: true }));
+    });
+  };
+
+  server.on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      console.log('Address in use, retrying...');
+      setTimeout(() => {
+        server.close();
+        connect();
+      }, 5000);
+    }
   });
+  connect();
 });
